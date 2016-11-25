c73ba3a7a364b7fe68d4875edb768e542836ca8c
Alex <alex@devico.io>
2016-11-25 12:20:10 +0200

    Command wrappers and sudo

--- src/adapter/createSshAdapter.js
+++ src/adapter/createSshAdapter.js
@@ -10,6 +10,10 @@ export function createSshAdapter(configuration) {
   let queue = [
 
   ]
+  configuration = {
+    ...configuration,
+    tryKeyboard: true
+  }
 
   async function executeQueue() {
     if (!queue.length) {
@@ -48,6 +52,48 @@ export function createSshAdapter(configuration) {
     err => console.log('Error', err)
   )
 
+  function generateCallback(resolve: Function, reject: Function): Function {
+    return function(error, result) {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(result)
+      }
+    }
+  }
+
+  async function execCommand(givenCommand: string, options: { cwd?: string, stdin?: string } = {}): Promise<{ stdout: string, stderr: string, code: number, signal: ?string }> {
+    let command = givenCommand
+    if (options.cwd) {
+      // NOTE: Output piping cd command to hide directory non-existent errors
+      command = `cd ${shellEscape([options.cwd])} 1> /dev/null 2> /dev/null; ${command}`
+    }
+    const output = { stdout: [], stderr: [] }
+    return await new Promise(function(resolve, reject) {
+      connection.connection.exec(command, options, generateCallback(function(stream) {
+        stream.on('data', function(chunk) {
+          chunk = chunk.toString()
+          if (chunk.indexOf('[sudo] password') !== -1) {
+            stream.write(configuration.password)
+            stream.end()
+          }
+          console.log(chunk)
+          output.stdout.push(chunk)
+        })
+        stream.stderr.on('data', function(chunk) {
+          output.stderr.push(chunk)
+        })
+        if (options.stdin) {
+          stream.write(options.stdin)
+          stream.end()
+        }
+        stream.on('close', function(code, signal) {
+          resolve({ code, signal, stdout: output.stdout.join('').trim(), stderr: output.stderr.join('').trim() })
+        })
+      }, reject))
+    })
+  }
+
   return {
     isReady: () => ready,
     execute(text, options = {}) {
@@ -63,6 +109,30 @@ export function createSshAdapter(configuration) {
           executeQueue()
         }
       })
+    },
+    wrapCommand(command, defaultOptions = {}) {
+      if (defaultOptions.sudo && configuration.username !== 'root') {
+        command = 'sudo ' + command
+        defaultOptions.pty = true
+      }
+      if (defaultOptions.env) {
+        command = Object.keys(defaultOptions.env).map(key => `${key}=${defaultOptions.env[key]}`).join(' ') + ' ' + command
+      }
+      const client = {
+        exec(instruction, args = [], options = {}) {
+          return connection.exec(command + ' ' + instruction, args, {...defaultOptions, ...options})
+        },
+        execCommand(instruction, options = {}) {
+          return execCommand(command + ' ' + instruction, {...defaultOptions, ...options})
+        }
+      }
+      if (defaultOptions.methods) {
+        defaultOptions.methods.forEach(method => {
+          client[method] = (instruction, options) => client.execCommand(method + ' ' + instruction, options)
+        })
+      }
+
+      return client
     },
     execCommand(...options) {
       return connection.execCommand(...options)
--- src/ubuntu/createAptWrapper.js
+++ src/ubuntu/createAptWrapper.js
@@ -3,6 +3,19 @@ const versionOutputRegex = new RegExp('[\\w]+[\\s]+([\\w\\-\\.]+)[\\s]+([\\w\\:\
 
 export function createAptWrapper(logger, ssh) {
   const aptLogger = logger.createSubLogger(`[[blue:Apt]]`)
+  const aptget = ssh.wrapCommand('apt-get', {
+    methods: ['install', 'update', 'upgrade', 'remove', 'purge'],
+    sudo: true,
+    env: {
+      DEBIAN_FRONTEND: 'noninteractive'
+    }
+  })
+  const dpkg = ssh.wrapCommand('dpkg', {
+    sudo: true,
+    env: {
+      DEBIAN_FRONTEND: 'noninteractive'
+    }
+  })
 
   async function execute(tool, command, argument = null) {
     try {
@@ -14,7 +27,11 @@ export function createAptWrapper(logger, ssh) {
 
   return {
     install(packages) {
-      return aptLogger.waitFor('Installing packages ' + packages.green, execute('apt-get', 'install', packages), 'Installed ' + packages.green)
+      return aptLogger.waitFor(
+        'Installing packages ' + packages.green,
+        aptget.install(packages + ' -y'),
+        'Installed ' + packages.green
+      )
     },
     async addRepository(repo) {
       return await aptLogger.waitFor(
@@ -24,16 +41,23 @@ export function createAptWrapper(logger, ssh) {
       )
     },
     update() {
-      return aptLogger.waitFor('Updating packages list', execute('apt-get', 'update'))
+      return aptLogger.waitFor('Updating packages list', aptget.update(' -y'))
     },
     upgrade() {
-      return aptLogger.waitFor('Upgrading packages to latest versions', execute('apt-get', 'upgrade'))
+      return aptLogger.waitFor('Upgrading packages to latest versions', aptget.upgrade(' -y'))
     },
     remove(packages) {
-      return aptLogger.waitFor(`Uninstalling packages [green:${packages}]`, execute('apt-get', 'remove', packages), 'Uninstalled ' + packages.green)
+      return aptLogger.waitFor(
+        `Uninstalling packages [green:${packages}]`,
+        aptget.remove(packages + ' -y'),
+        'Uninstalled ' + packages.green
+      )
     },
     async getInfo(packageName) {
-      const {stdout: output} = await aptLogger.waitFor('Getting info about package ' + packageName.green, execute('dpkg -l', packageName, '| grep ' + packageName))
+      const {stdout: output} = await aptLogger.waitFor(
+        'Getting info about package ' + packageName.green,
+        dpkg.exec('dpkg -l ' + packageName + ' | sudo grep ' + packageName)
+      )
       if (versionOutputRegex.test(output)) {
         const [, name, version, arch, description] = output.match(versionOutputRegex)
         aptLogger.log('  ' + packageName.green + ' already installed with version ' + version.yellow)
@@ -52,7 +76,10 @@ export function createAptWrapper(logger, ssh) {
       }
     },
     purge(packages) {
-      return aptLogger.waitFor('Purge packages ' + packages.green, execute('apt-get', 'purge', packages))
+      return aptLogger.waitFor(
+        'Purge packages ' + packages.green,
+        aptget.purge(packages + ' -y')
+      )
     }
   }
 }
